a0508e7240e69fd541a96337d02831f4605d3668
Aniket Yadav <aniketyadav690@gmail.com>
2022-10-22 01:41:49 +0530

    still in progress

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,41 +1,97 @@
 import React, { useEffect, useState } from 'react'
 import Line from './Lines';
 
-import styles from "./index.module.css"
+import "./style.css"
 
 const Board = ({ solution }) => {
 //console.log(solution)
 
 const[currentGuess, setCurrentGuess] = useState("");
 const[guesses, setGuesses] = useState(new Array(6).fill(null));
+const[isGameOver, setIsGameOver] = useState(false);
 
  useEffect(() => {
-   const typeHandler = (event) => {
+   const typeHandler = (e) => {
      //console.log(e.key);
-    setCurrentGuess((oldGuess) => oldGuess + event.key);
+
+     if(isGameOver){
+      return;
+     }
+
+     
+     
+    if(e.key === "Backspace"){
+      setCurrentGuess(currentGuess.slice(0, -1)); //this will help you to delete or earse your previous character
+      return;
+    }
+    
+     if(e.key === "Enter"){
+      if(currentGuess.length !== 5){
+        return;
+      }
+
+      const newGuesses = [...guesses];
+      newGuesses[guesses.findIndex((val) => val === null)] = currentGuess;
+      setGuesses(newGuesses);
+      setCurrentGuess("");
+
+
+      const isCorrect = solution === currentGuess;
+      if(isCorrect){
+        setIsGameOver(true);
+      }
+      
+     }
+
+
+     if(currentGuess.length >= 5){
+      return;
+     }
+
+
+    const isLetter = e.key.match(/^[a-z]{1}$/) != null; //this regex is to check if we have written letter
+
+    if(isLetter){
+      setCurrentGuess((oldGuess) => oldGuess + e.key);
+    }
+    
   }
 
   window.addEventListener('keypress', typeHandler);
 
   return () => window.removeEventListener('keypress', typeHandler);
 
- },[currentGuess]);
+ },[currentGuess, isGameOver, solution]);
 
 
- const lines = guesses.map((guess, i) => {
+//  const lines = guesses.map((guess, i) => {
   
-  let isCurrentGuess = guesses.findIndex((val) => val !== null); 
+//   let isCurrentGuess = guesses.findIndex((val) => val === null) === i; 
+//   //console.log(isCurrentGuess)
+
+//     return(
+//       <div key={i} className={styles.line}>
+//       <Line guess = { isCurrentGuess ? currentGuess : guess || ""}/>
+//       </div>
+//     )
+//  });
+
+  return (
+    <div className="board">
+      { guesses.map((guess, i) => {
+  
+  let isCurrentGuess = guesses.findIndex((val) => val === null) === i; 
   //console.log(isCurrentGuess)
 
     return(
-      <div key={i} className={styles.line}>
-      <Line guess = { isCurrentGuess ? currentGuess : guess || ""}/>
+      <div key={i} className="line">
+      <Line guess = { isCurrentGuess ? currentGuess : guess || ""}
+      isFinal = {!isCurrentGuess && guess !== null} 
+      solution = {solution}/>
       </div>
     )
- });
-
-  return (
-    <div className={styles.board}>{lines}</div>
+  })};
+    </div>
   )
 }
 
--- src/components/Lines.js
+++ src/components/Lines.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import styles from "./index.module.css"
+import  "./style.css"
 
 let WORD_SIZE = 5;
 
-const Line = ({ guess }) => {
+const Line = ({ guess, isFinal, solution }) => {
 let tiles = [];
 
   for(let i = 0; i < WORD_SIZE; i++){
    let char = guess[i];
-   tiles.push(<div key = {i} className={styles.tile}>{char}</div>);
+
+   let className = 'tile'
+   
+   if(isFinal){
+    if(char === solution[i]){
+        className += 'correct'
+    }else if(solution.includes(char)){
+       className += 'close'
+    }else{
+      className += 'incorrect'
+    }
+   }
+
+   tiles.push(<div key = {i} className= {className}>{char}</div>);
   }
 
 
   return (
-    <div className = {styles.line}>{tiles}</div>
+    <div className = "line">{tiles}</div>
   )
 }
 
