381d099c49900e00c0869099175eb60ac3676eed
Aniket Yadav <aniketyadav690@gmail.com>
2022-10-21 19:45:46 +0530

    random words generated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import WORDS from "./data/Words";
+
+//const API = "https://api.frontendexpert.io/api/fe/wordle-words"
 
 function App() {
+ //console.log(WORDS)
+ const[words, setWords] = useState(WORDS);
+ const[solution, setSolution] = useState(""); 
+
+useEffect(() => {
+  const fetchData = async () => {
+    await setWords(WORDS);
+    setSolution(words[Math.floor(Math.random() * words.length)])
+    // const randomWord = data[Math.floor(Math.random() * data.length)];
+    // setSolution(randomWord)
+  }
+  fetchData();
+}, []);
+
   return (
     <div className = "app">
-   Hello
-    
+     
+    {solution}
     </div>
   );
 }
